6ca1f1634cab402ccc54ba0896754e10b0740170
Caio Rodrigues <caio.contato03@gmail.com>
2025-01-29 21:55:34 +0000

    Criando o printf e scanf

---
+++ Paises.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int estados [20];
+    char jogo [50];
+    char pais1, pais2, pais3, pais4 [60];
+    
+    printf("Bem-Vindo ao jogo Super Trunfo.\n");
+    printf("Por favor, digite 4 paises: ");
+    scanf("%s %s %s %s", pais1, pais2, pais3, pais4);
+
+}
