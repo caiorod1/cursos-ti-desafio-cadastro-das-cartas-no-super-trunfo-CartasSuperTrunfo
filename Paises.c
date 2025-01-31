0056d1fd9ab3efd07dd516b10e0885c8c93d49d6
Caio Rodrigues <caio.contato03@gmail.com>
2025-01-31 13:52:18 +0000

    Atualizado 31/01

--- Paises.c
+++ Paises.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
 
-//criando as variaveis
-
 int main(){
-    int estados;
-    int jogo [50];
-    char est1 [4]= "A01", est2 [4]= "B02", est3[4] = "C03", est4[4] = "D04", est5[4] = "E05", est6[4] = "F06", est7[4] = "G07", est8 [4] = "H08";   // coloquei estados como est para nao ficar muito grande na hora de codar
-    char est9 [4] = "A01", est10[4] = "B02", est11[4]= "C03", est12[4] = "D04", est13[4] = "E05", est14 [4]= "F06", est15 [4]= "G07", est16 [4]= "H08";
-    
-//sai de mensagens falando um pouco sobre o jogo.
+   /*
+   Criando as variaveis para armazenar os cadastros da carta do usuario e logo após mostrar os atributos que o mesmo adicinou
+   */
+    char codigo [5]; //codigo da cidade
+    char nome [50]; //nome da cidade
+    double populacao; // população da cidade
+    float area;  // area da cidade
+    double PIB; //PIB da cidade 
+    int turistico; // pontos turisticos da cidade
 
 printf("Bem-Vindo ao jogo Super Trunfo Paises.\n");
-printf("Para começar o jogo, iremos separar 1 pais, cada pai com 8 estados e cada estados com 4 cidades.\n");
+printf("Para iniciarmos o jogo, você irá precisar colocar os seguintes dados da cidade: Código da cidade, nome, população, área, PIB, número de pontos turísticos.\n");
 
-printf("jogador 1:\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n", est1, est2, est3, est4, est5, est6, est7, est8);
+printf("Insira o código da cidade: ");
+scanf("%s", codigo);
 
-printf("\nAgora vamos aprensetar as cartas aléatorias do jogador 2\n\n");
-printf("jogador2:\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n", est9, est10, est11, est12, est13, est14, est15, est16);
+printf("Insira o nome da cidade: ");
+scanf("%s", nome);
 
+printf("Insira a população da cidade: ");
+scanf("%lf", &populacao);
+
+printf("Insira a área da cidade: ");
+scanf("%f", &area);
+
+printf("Insira o PIB: ");
+scanf("%lf", &PIB);
+
+printf("Existem quantos pontos turisticos da cidade: ");
+scanf("%d", &turistico);
+
+printf("Pronto, agora vou está te mostrando os atributos da sua carta.\n");
+
+printf("Código: %s\n", codigo);
+printf("Nome da cidade: %s\n", nome);
+printf("População: %.0lf\n", populacao);
+printf("Área: %.2f km²\n", area);
+printf("PIB: %.2lf\n", PIB);
+printf("Pontos turisticos: %d\n", turistico);
 
 return 0;
+
 
 }
